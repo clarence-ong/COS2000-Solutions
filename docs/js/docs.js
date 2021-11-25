dcb46d0afa9e8b413c417283e2abd4eea1d8ae60
clarence-ong <clarence@LAPTOP-PS5NF8JF.localdomain>
2021-11-25 11:30:25 +0800

    fifth commit

---
+++ docs/js/docs.js
@@ -0,0 +1,15 @@
+var analysisArray = ['first-glance', 'station-map', 'time-of-day-trends', 'seasonal-trends']
+var analysisDict = {'span-style-color:blue-Lab-Exercise-1-Solutions-(Total-Marks:-12)-span-1': 'LabEx1_Solutions', 'span-style-color:blue-Lab-Exercise-2-Solutions-(Total-Marks:-25)-span-1': 'LabEx2_Solutions', 'span-style-color:blue-Lab-Exercise-3-Solutions-(Total-Marks:-35)-span-1': 'LabEx3_Solutions', 'span-style-color:blue-Lab-Exercise-4-Solutions-(Total-Marks:-20)-span-1': 'LabEx4_Solutions', 'span-style-color:blue-Lab-Exercise-5-Solutions-(Total-Marks:-20)-span-1': 'LabEx5_Solutions', 'span-style-color:blue-Lab-Exercise-7-Solutions-(Total-Marks:-20)-span-1': 'LabEx7_Solutions', 'span-style-color:blue-Lab-Exercise-8-Solutions-(Total-Marks:-20)-span-1': 'LabEx8_Solutions', 'span-style-color:blue-Lab-Exercise-9-Solutions-(Total-Marks:-20)-span-1': 'LabEx9_Solutions', 'A-first-glance-at-the-Divvy-data-1': 'first-glance', 'Divvy-usage-over-the-year-1': 'seasonal-trends', 'A-map-of-the-Divvy-stations-in-Chicago-1': 'station-map', 'Divvy-usage-by-time-of-day-1': 'time-of-day-trends'}
+var analysisArrayMap = {'LabEx1_Solutions': '<span style = "color:bl ... : 12)</span>', 'LabEx2_Solutions': '<span style = "color:bl ... : 25)</span>', 'LabEx3_Solutions': '<span style = "color:bl ... : 35)</span>', 'LabEx4_Solutions': '<span style = "color:bl ... : 20)</span>', 'LabEx5_Solutions': '<span style = "color:bl ... : 20)</span>', 'LabEx7_Solutions': '<span style = "color:bl ... : 20)</span>', 'LabEx8_Solutions': '<span style = "color:bl ... : 20)</span>', 'LabEx9_Solutions': '<span style = "color:bl ... : 20)</span>', 'first-glance': 'A first glance at the Divvy data', 'seasonal-trends': 'Divvy usage over the year', 'station-map': 'A map of the Divvy stat ... s in Chicago', 'time-of-day-trends': 'Divvy usage by time-of-day'}
+var setupArray = []
+var setupDict = {}
+var setupArrayMap = {}
+var licenseArray = []
+var licenseDict = {}
+var licenseArrayMap = {}
+var notesArray = []
+var notesDict = {}
+var notesArrayMap = {}
+var protectedArray = ['protected_page']
+var protectedDict = {'This-page-is-password-protected-1': 'protected_page'}
+var protectedArrayMap = {'protected_page': 'This page is password protected'}
